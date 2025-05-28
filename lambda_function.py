e069d86e00f9335f441f9d0906b3e7fbba1fee64
TimoH447 <timo.hauser66@gmail.com>
2025-05-28 17:33:31 +0200

    add lambda related code

---
+++ lambda_function.py
@@ -0,0 +1,118 @@
+import json
+import boto3
+from botocore.exceptions import ClientError
+import os
+import sys
+import uuid
+from urllib.parse import unquote_plus
+from PIL import Image
+import PIL.Image
+
+from src.visualize import LatticeImage
+from src.lattice import StateLattice
+from src.knotdiagram import KnotDiagram
+            
+s3_client = boto3.client('s3')
+            
+
+trefoil_pd = [(6, 4, 1,3), (4, 2, 5, 1), (2, 6, 3, 5)]
+link_pd = [
+    (1,5,4,16),
+    (16,4,15,3),
+    (3,13,2,12),
+    (2,11,1,12),
+    (15,7,14,8),
+    (8,14,9,13),
+    (5,17,6,22),
+    (22,6,21,7),
+    (9,18,10,19),
+    (19,10,20,11),
+    (21,17,20,18)
+]
+knot_8_8 = [
+    (1,7,2,6),
+    (2,5,3,6),
+    (5,12,4,11),
+    (4,10,3,11),
+    (1,8,16,7),
+    (16,13,15,12),
+    (13,8,14,9),
+    (14,10,15,9)
+]
+fig_8=[
+    (4,8,3,1),
+    (2,7,1,6),
+    (8,4,7,5),
+    (6,3,5,2),
+]
+paper_knot = [
+    (1,13,20,14),
+    (19,9,18,10),
+    (17,7,16,8),
+    (15,1,14,2),
+    (13,5,12,6),
+    (11,15,10,16),
+    (9,17,8,18),
+    (7,3,6,4),
+    (5,11,4,12),
+    (3,19,2,20)
+]
+def main(parsed_pd_notation,fixed_segment,filename):
+    diagram = KnotDiagram(parsed_pd_notation)
+    lattice = StateLattice(diagram,fixed_segment)
+    lattice.build_lattice()
+    lattice_image = LatticeImage(lattice, image_size=(512, 1024), padding=(10, 20), text_size=9)
+    lattice_image.draw_lattice(filename)
+    results = {
+        "number_of_states": len(lattice.nodes),
+    }
+    return results
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        print(e)
+        return False
+    return True
+
+def parse_input(body):
+    pd_notation = body.get('pd_notation', None)
+    fixed_segment = body.get('fixed_segment', None)
+    return (pd_notation, fixed_segment)
+
+def lambda_handler(event, context):
+    """
+    AWS Lambda handler function.
+    """
+    body = json.loads(event.get('body', '{}'))
+    pd_notation,fixed_segment = parse_input(body)
+    filename = "lattice.png"
+    bucket = 'lambdaimage-testbucket'
+    result = main(pd_notation, fixed_segment, '/tmp/'+filename)
+    upload_file('/tmp/'+ filename , bucket ,filename)
+    # Generate URL valid for 5 minutes
+    s3 = boto3.client('s3')
+    url = s3.generate_presigned_url('get_object', 
+        Params={'Bucket': bucket, 'Key': filename},
+        ExpiresIn=300
+    )
+    result['image_url'] = url
+    return {
+        'statusCode': 200,
+        'body': json.dumps(result)
+    }
--- src/visualize.py
+++ src/visualize.py
@@ -57,7 +57,7 @@ class LatticeImage:
             (end_node_coords[0], end_node_coords[1] - self.text_size)
         ]
 
-    def draw_lattice(self):
+    def draw_lattice(self,filename=None):
         self.set_node_coordinates()
         im = Image.new('1', self.image_size, "white")
         draw = ImageDraw.Draw(im)
@@ -67,53 +67,10 @@ class LatticeImage:
             node_coords = self.get_node_coordinates(node)
 
             draw.text((node_coords[0], node_coords[1]), node.transpositions.string, fill="black",anchor="mm",font_size=self.text_size)
-        im.save("lattice.png")
-
-def node_coordinate_to_img_position(node_coordinate, layer_length, layer_width, padding_x, padding_y):
-    """
-    Convert node coordinates to image pixel positions.
-    """
-    x = padding_x + node_coordinate[0] * layer_width
-    y = padding_y + node_coordinate[1] * layer_length 
-    return (x, y)
-
-def set_node_coordinates(nodes):
-    length_list_x = [node.get_length() for node in nodes]
-    for node in nodes:
-        node.position = (length_list_x.count(node.get_length()),node.get_length())
-        length_list_x.remove(node.get_length())
-
-def draw_lattice(nodes,edges):
-    set_node_coordinates(nodes)
-    length_list_x = [node.get_length() for node in nodes]
-    depth = nodes[-1].get_length()
-    img_width = 512
-    img_height = 1024
-    padding_x = 10
-    padding_y = 20
-    padding_text_y = 8
-    im = Image.new('1',(img_width,img_height),"white")
-    draw = ImageDraw.Draw(im)
-    layer_length  = math.floor((img_height - 2*padding_y)/(depth+1))
-    for edge in edges:
-        start_node_index = nodes.index(edge[0])
-        end_node_index = nodes.index(edge[1])
-
-        layer = edge[0].get_length()
-        number_of_layer_nodes_layer_start = length_list_x.count(layer)
-        layer_width_start = math.floor((img_width - 2*padding_x)/(number_of_layer_nodes_layer_start+1))
-
-        number_of_layer_nodes_layer_target = length_list_x.count(layer+1)
-        layer_width_target = math.floor((img_width - 2*padding_x)/(number_of_layer_nodes_layer_target+1))
-
-        start_pos = node_coordinate_to_img_position(nodes[start_node_index].position, layer_length,layer_width_start, padding_x, padding_y)
-        end_pos = node_coordinate_to_img_position(nodes[end_node_index].position, layer_length, layer_width_target, padding_x, padding_y)
-        start_pos = (start_pos[0], start_pos[1] + padding_text_y)
-        end_pos = (end_pos[0], end_pos[1] - padding_text_y)
-        draw.line([start_pos,end_pos],fill="black",width=1)
-
-        
-    im.save("lattice.png")
+        if filename==None:
+            im.save("lattice.png")
+        else:
+            im.save(filename)
 
 
 
